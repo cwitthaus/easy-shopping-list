ca9f4442d811ae8ec15e0464ccfd9a783c4e7cf1
Cyrus Witthaus <cwitthaus@widen.com>
2012-12-13 08:00:09 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyShoppingList::Application.config.secret_token = '22182e4bc40ca3cc99cc4868430e77cfe58bfeba47a801badca7c910ec5ea727178bb4ffe4a1cca36ada0bd307d830e08491e9a4650d08b72dbc492ed6b055be'
